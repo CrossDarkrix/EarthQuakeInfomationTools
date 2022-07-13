5e0a9853f8338e37f53dda5c19106bea558ea669
CrossDarkrix <crosseddarkrix@gmail.com>
2022-07-13 09:57:48 +0900

    コードの軽量化をしました。

--- EarthQuakeInfomationTool.py
+++ EarthQuakeInfomationTool.py
@@ -1,100 +1,70 @@
 # -*- coding: utf-8 -*-
 """
 Colorful Getting Earthquake Infomation from YahooJapan
-Version: 3.2b
+Version: 3.3b
 Author: DarkRix
 """
 
-from colorama import init as Starting
-from colorama import Fore, Back
-import urllib.request, re
-from bs4 import BeautifulSoup
-Starting()
+import bs4, colorama, re, urllib.request
 
-def replace_word(EqData):
-    replaceword0 = '発生時刻\n\n'
-    replaceword1 = '震源地\n\n\n'
-    replaceword2 = '最大震度\n'
-    replaceword3 = 'マグニチュード\n'
-    replaceword4 = '深さ\n'
-    replaceword5 = '緯度/経度\n'
-    replaceword6 = '情報\n\n\n\n\n'
-    replaceword7 = '。\n'
-    replaceword8 = '\n\n\n'
-    replaceword9 = '詳細を見る'
-    replaceword10 = '\n\n'
-    replaceword11 = '・'
-    replaceword12 = '\n\n\n'
-    replaceword13 = '>>津波情報を見る'
-    replaceword14 = '\n\n\n\n\n'
-    replaceword15 = '\n\n\n'
-    replaceword16 = 'M '
-    replaceword17 = '。'
-    replacedword0 = ''
-    replacedword1 = '・発生時刻: '
-    replacedword2 = '・震源地: '
-    replacedword3 = '・最大震度: '
-    replacedword4 = '・地震の規模: M'
-    replacedword5 = '・深さ: '
-    replacedword6 = '・緯度/経度: '
-    replacedword7 = '・津波情報: '
-    replacedword8 = ': '
-    replacedword9 = 'M'
-    replacedword10 = '\n'
-    replacedword11 = '\n・'
+def _r(d):
+    _r0 = '発生時刻'
+    _r1 = '震源地\n'
+    _r2 = '最大震度\n'
+    _r3 = 'マグニチュード\n'
+    _r4 = '深さ\n'
+    _r5 = '緯度/経度\n'
+    _r6 = '情報\n'
+    _rd0 = '・発生時刻: '
+    _rd1 = '\n\n・震源地: '
+    _rd2 = '\n\n・最大震度: '
+    _rd3 = '\n・地震の規模: M'
+    _rd4 = '\n・深さ: '
+    _rd5 = '\n・緯度/経度: '
+    _rd6 = '\n・津波情報: '
+    _sw = '。'
+    _eq = re.sub('([ ])', '', d)
+    _eq1 = re.sub('(\n\n)','', _eq)
+    _eq2 = _eq1.replace(_r0, _rd0)
+    _eq3 = _eq2.replace(_r1, _rd1)
+    _eq4 = _eq3.replace(_r2, _rd2)
+    _eq5 = _eq4.replace(_r3, _rd3)
+    _eq6 = _eq5.replace(_r4, _rd4)
+    _eq7 = _eq6.replace(_r5, _rd5)
+    _eq8 = _eq7.replace(_r6, _rd6).split(_sw)[0]
 
-    reps0 = re.sub('([ ])', '', EqData)
-    reps1 = reps0.replace(replaceword0,replacedword1)
-    reps2 = reps1.replace(replaceword1,replacedword2)
-    reps3 = reps2.replace(replaceword2,replacedword3)
-    reps4 = reps3.replace(replaceword3,replacedword4)
-    reps5 = reps4.replace(replaceword4,replacedword5)
-    reps6 = reps5.replace(replaceword5,replacedword6)
-    reps7 = reps6.replace(replaceword6,replacedword7)
-    reps8 = reps7.replace(replaceword7,replacedword0)
-    reps9 = reps8.replace(replaceword8,replacedword10)
-    reps10 = reps9.replace(replaceword9,replacedword0)
-    reps11 = reps10.replace(replaceword10,replacedword10)
-    reps12 = reps11.replace(replaceword11,replacedword11)
-    reps13 = reps12.replace(replaceword12,replacedword0)
-    reps14 = reps13.replace(replaceword13,replacedword0) 
-    reps15 = reps14.replace(replaceword14,replacedword0) 
-    reps16 = reps15.replace(replaceword15,replacedword0) 
-    reps17 = reps16.replace(replaceword16,replacedword9) 
-    reps18 = reps17.replace(replaceword17,replacedword0)
-    
-    return reps18
+    return _eq8
 
 def main():
-    Yahoo_URL = "https://typhoon.yahoo.co.jp/weather/earthquake/"
-    user_agent = 'Mozilla/5.0 (Linux; Android 7.1.2; en-la) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/52.0.2743.100 Mobile Safari/537.36 YJApp-ANDROID jp.co.yahoo.android.yjtop/13.91.1'
-    response = urllib.request.urlopen(urllib.request.Request(Yahoo_URL, headers={'User-Agent': user_agent}))
-    EqDetail = BeautifulSoup(response,features="html5lib").select("div.eqDetail")
-    
-    Detect_Shindo = str(EqDetail)
-    if '<td>1</td>' in Detect_Shindo:
-        print(Fore.WHITE + Back.BLACK)
-    if '<td>2</td>' in Detect_Shindo:
-        print(Fore.WHITE + Back.BLUE)
-    if '<td>3</td>' in Detect_Shindo:
-        print(Fore.BLACK + Back.GREEN)
-    if '<td>4</td>' in Detect_Shindo:
-        print(Fore.BLACK + Back.YELLOW)
-    if '<td>5弱</td>' in Detect_Shindo:
-        print(Fore.WHITE + Back.RED)
-    if '<td>5強</td>' in Detect_Shindo:
-        print(Fore.WHITE + Back.RED)
-    if '<td>6弱</td>' in Detect_Shindo:
-        print(Fore.WHITE + Back.RED)
-    if '<td>6強</td>' in Detect_Shindo:
-        print(Fore.WHITE + Back.RED)
-    if '<td>7</td>' in Detect_Shindo:
-        print(Fore.WHITE + Back.MAGENTA)
-    
-    for EqInfo in EqDetail:
-        print(replace_word(EqInfo.getText()), end="")
+    _u = "https://typhoon.yahoo.co.jp/weather/earthquake/"
+    _ua = 'Mozilla/5.0 (Linux; U; Android 8.0; en-la; Nexus Build/JPG991) AppleWebKit/511.2 (KHTML, like Gecko) Version/5.0 Mobile/11S444 YJApp-ANDROID jp.co.yahoo.android.yjtop/4.01.1.5'
+    _d = urllib.request.urlopen(urllib.request.Request(_u, headers={'User-Agent': _ua}))
+    _t = bs4.BeautifulSoup(_d, features="html5lib").select("div.eqDetail")
+    colorama.init()
+
+    _dq = str(_t)
+    if '<td>1</td>' in _dq:
+        print(colorama.Fore.WHITE + colorama.Back.BLACK)
+    if '<td>2</td>' in _dq:
+        print(colorama.Fore.WHITE + colorama.Back.BLUE)
+    if '<td>3</td>' in _dq:
+        print(colorama.Fore.BLACK + colorama.Back.GREEN)
+    if '<td>4</td>' in _dq:
+        print(colorama.Fore.BLACK + colorama.Back.YELLOW)
+    if '<td>5弱</td>' in _dq:
+        print(colorama.Fore.WHITE + colorama.Back.RED)
+    if '<td>5強</td>' in _dq:
+        print(colorama.Fore.WHITE + colorama.Back.RED)
+    if '<td>6弱</td>' in _dq:
+        print(colorama.Fore.WHITE + colorama.Back.RED)
+    if '<td>6強</td>' in _dq:
+        print(colorama.Fore.WHITE + colorama.Back.RED)
+    if '<td>7</td>' in _dq:
+        print(colorama.Fore.WHITE + colorama.Back.MAGENTA)
+
+    print(''.join([_r(_e.getText()) for _e in _t]), end='')
+    print(colorama.Fore.RESET + colorama.Back.RESET)
     print("\n")
-    print(Fore.RESET + Back.RESET)
 
 if __name__ == '__main__':
     main()
