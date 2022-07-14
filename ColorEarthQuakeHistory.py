e3e9312288a78967e4e222544d49862d60cf4bb5
CrossDarkrix <crosseddarkrix@gmail.com>
2022-07-15 05:26:42 +0900

    remove space.

--- ColorEarthQuakeHistory.py
+++ ColorEarthQuakeHistory.py
@@ -10,9 +10,9 @@ import bs4, colorama, urllib.request
 
 def main():
     _u = "https://typhoon.yahoo.co.jp/weather/earthquake/"
-    _ua = 'Mozilla/5.0 (Linux; U; Android 8.0; en-la; Nexus Build/JPG991) AppleWebKit/511.2 (KHTML, like Gecko) Version/5.0 Mobile/11S444 YJApp-ANDROID jp.co.yahoo.android.yjtop/4.01.1.5' 
+    _ua = 'Mozilla/5.0 (Linux; U; Android 8.0; en-la; Nexus Build/JPG991) AppleWebKit/511.2 (KHTML, like Gecko) Version/5.0 Mobile/11S444 YJApp-ANDROID jp.co.yahoo.android.yjtop/4.01.1.5'
     _g = urllib.request.urlopen(urllib.request.Request(_u, headers={'User-Agent': _ua}), timeout=1000)
-    _h = bs4.BeautifulSoup(_g, features="html5lib") 
+    _h = bs4.BeautifulSoup(_g, features="html5lib")
     _es = [_a.getText().split('  ')[0].split('\n')[0] for _a in _h.select("dd.subText")]
     _et = [_b.getText().split('  ')[0].split('\n')[0] for _b in _h.select("dt.title") if '震源地：' in _b.getText()]
     _esc = [_c.getText().split('    ')[0].split('\n')[0] + _c.getText().split('    ')[1].split('\n')[0].replace('M', ' (M').replace(_c.getText().split('    ')[1].split('\n')[0][-1], _c.getText().split('    ')[1].split('\n')[0][-1] + ')') for _c in _h.select('dd.eqScale')]
